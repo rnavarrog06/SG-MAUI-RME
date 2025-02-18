17267985b0b90c1a589102c6c8bd390564734df3
gitManuDam <manumunozr98@gmail.com>
2025-02-18 09:32:49 +0100

    1.Configuracion Creacion de la clase constantes

---
+++ SG-MAUI-RME/Constantes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG_MAUI_RME
+{
+    public class Constantes
+    {
+        private const string DATABASE_NAME = "SQLiteEjemplo.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DataBasePath
+        {
+            get
+            {
+                return Path.Combine(FileSystem.AppDataDirectory, DATABASE_NAME);
+            }
+
+        }
+
+    }
+
+}
